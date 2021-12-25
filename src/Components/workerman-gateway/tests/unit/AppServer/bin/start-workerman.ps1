500884cef65eac66345eecca3c9889cf94029f12
Yurun <admin@yurunsoft.com>
2021-12-25 13:03:15 +0800

    更新

--- src/Components/workerman-gateway/tests/unit/AppServer/bin/start-workerman.ps1
+++ src/Components/workerman-gateway/tests/unit/AppServer/bin/start-workerman.ps1
@@ -4,12 +4,15 @@ $__DIR__ = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 
 $procss = Start-Process -PassThru -FilePath "php" -ArgumentList "$__DIR__\workerman workerman/start --name register"
 $procss.Id | Out-File $__DIR__/register.pid
+Start-Sleep -m 1000
 
 $procss = Start-Process -PassThru -FilePath "php" -ArgumentList "$__DIR__\workerman workerman/start --name gateway"
 $procss.Id | Out-File $__DIR__/gateway.pid
+Start-Sleep -m 1000
 
 $procss = Start-Process -PassThru -FilePath "php" -ArgumentList "$__DIR__\workerman workerman/start --name websocket"
 $procss.Id | Out-File $__DIR__/websocket.pid
+Start-Sleep -m 3000
 
 $procss = Start-Process -PassThru -FilePath "php" -ArgumentList "$__DIR__\workerman workerman/start --name http"
 $procss.Id | Out-File $__DIR__/http.pid
